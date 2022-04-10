ecea2a168767925f8da6598eed5403d4927fd455
Anders Blenstrup-Pedersen <abp-git@ryuu.technology>
2022-04-10 23:36:17 +0900

    update - adding test

--- src/helper.rs
+++ src/helper.rs
@@ -9,3 +9,30 @@ fn get_from_hash_set(hash: HashSet<&str>) -> crate::Result<simple_serde::Content
     }
     Err(FailedToMatchAnyContentType)
 }
+
+#[cfg(test)]
+mod test {
+    use crate::helper::get_from_hash_set;
+    use std::collections::HashSet;
+
+    #[test]
+    fn test_get_from_hash_set() {
+        let mut hash_set = HashSet::new();
+        hash_set.insert("blabla");
+        hash_set.insert("foo");
+        hash_set.insert("bar");
+        hash_set.insert("yaml");
+
+        assert!(get_from_hash_set(hash_set).is_ok())
+    }
+
+    #[test]
+    fn test_get_from_hash_set_failure() {
+        let mut hash_set = HashSet::new();
+        hash_set.insert("blabla");
+        hash_set.insert("foo");
+        hash_set.insert("bar");
+
+        assert!(!get_from_hash_set(hash_set).is_ok())
+    }
+}
